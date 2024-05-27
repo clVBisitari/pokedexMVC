1c9fbc230a4743b3cecd8c345197c1fb420cb2cf
Mariano Sald <odioescribirel@hotmail.com>
2024-05-27 17:56:25 -0300

    users table back to user and dB create sql file

--- model/UserModel.php
+++ model/UserModel.php
@@ -12,26 +12,26 @@ class UserModel
     }
 
     public function getUser($id){
-        return $this->database->query("SELECT * FROM USERS WHERE id = '$id'");
+        return $this->database->query("SELECT * FROM USER WHERE id = '$id'");
     }
     public function writeSession($id, $isActive, $sessionId){
 
-        return $this->database->execute("UPDATE `users` SET `isActive` = $isActive, `sessionId` = '$sessionId' WHERE `id` = $id ");
+        return $this->database->execute("UPDATE `user` SET `isActive` = $isActive, `sessionId` = '$sessionId' WHERE `id` = $id ");
     }
     public function closeSession($id = null, $username){
         var_dump($id, $username);
-        $wasSuccessful = $this->database->execute("UPDATE `users` SET `isActive` = false WHERE `id` = $id OR `username` = '$username'");
+        $wasSuccessful = $this->database->execute("UPDATE `user` SET `isActive` = false WHERE `id` = $id OR `username` = '$username'");
 
         return $wasSuccessful;
     }
     public function addUser($userName, $password)
     {
-        return $this->database->execute("INSERT INTO `users`(`userName`, `password`) VALUES ( '$userName' , '$password')");
+        return $this->database->execute("INSERT INTO `user`(`userName`, `password`) VALUES ( '$userName' , '$password')");
     }
 
     public function getUserForNameAndPassword($username, $password){
 
-        $user = $this->database->query("SELECT * FROM users WHERE username ='$username' && password = '$password'");
+        $user = $this->database->query("SELECT * FROM user WHERE username ='$username' && password = '$password'");
         return $user;
 }
 }
