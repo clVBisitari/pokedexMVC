af51b39d0218836a8318f5979850ffe2dbe0474a
Mariano Sald <odioescribirel@hotmail.com>
2024-05-26 22:02:41 -0300

    cambios en header, y pokedexController. falta organizar un toq

--- controller/PokedexController.php
+++ controller/PokedexController.php
@@ -2,20 +2,50 @@
 
 class PokedexController
 {
-    private $model;
+    private $pokemonModel;
+    private $userModel;
     private $presenter;
 
-    public function __construct($model, $presenter)
+    public function __construct($pokemonModel, $userModel, $presenter)
     {
-        $this->model = $model;
+        $this->pokemonModel = $pokemonModel;
+        $this->userModel = $userModel;
         $this->presenter = $presenter;
     }
 
     public function get()
     {
-        $pokemons = $this->model->getPokemons();
-        $this->presenter->render("view/pokedexView.mustache", ["pokemons" => $pokemons]);
+        $pokemons = $this->pokemonModel->getPokemons();
+
+        if (isset($_POST['submit'])) {
+//            var_dump($_SERVER['REQUEST_METHOD']);
+            session_start();
+            $username = $_POST["user"];
+            $password = $_POST["pass"];
+            $user = $this->userModel->getUserForNameAndPassword($username, $password);
+//            var_dump( $user);
+
+            if ($user && $user[0]["password"] == $password) {
+
+//                $_SESSION["login"] = $username;
+//                $_SESSION["password"] = $password;
+
+                $this->presenter->render("view/pokedexView.mustache", ["pokemons" => $pokemons, "loggedIn" => true, "username" => $username]);
+            }
+            else {
+                $this->presenter->render("view/pokedexView.mustache", ["pokemons" => $pokemons, "errorUserOrPass" => "Credenciales incorrectas", "loggedIn" => false]);
+//                $this->presenter->render("/header.mustache", ["errorUserOrPass" => "Credenciales incorrectas", "loggedIn" => false]);
+            }
+
+        } else {
+            session_destroy();
+            $this->presenter->render("view/pokedexView.mustache", ["pokemons" => $pokemons]);
+        }
+    }
+
+    public function details(){
 
     }
+
 
 }
